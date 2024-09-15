e836885397a5553cd127809d94a7936905828e98
André Yermak Naumenko <asyermak@gmail.com>
2024-09-15 15:59:49 +0200

    Solution for incompatible versions v2

--- src/main/java/ub/andrewyernau/BloodEffect.java
+++ src/main/java/ub/andrewyernau/BloodEffect.java
@@ -30,81 +30,92 @@ public class BloodEffect implements Listener {
         }
 
         if (plugin.getConfig().getBoolean("passive.Enabled")) {
-            entityBloodMap.put(EntityType.ALLAY, "passive-mob.Wither");
-            entityBloodMap.put(EntityType.BAT, "passive-mobs.Bat");
-            entityBloodMap.put(EntityType.CAT, "passive-mobs.Cat");
-            entityBloodMap.put(EntityType.CHICKEN, "passive-mobs.Chicken");
-            entityBloodMap.put(EntityType.COD, "passive-mobs.Cod");
-            entityBloodMap.put(EntityType.COW, "passive-mobs.Cow");
-            entityBloodMap.put(EntityType.DONKEY, "passive-mobs.Donkey");
-            entityBloodMap.put(EntityType.FOX, "passive-mobs.Fox");
-            entityBloodMap.put(EntityType.GLOW_SQUID, "passive-mobs.GlowingSquid");
-            entityBloodMap.put(EntityType.HORSE, "passive-mobs.Horse");
-            entityBloodMap.put(EntityType.MULE, "passive-mobs.Mule");
-            entityBloodMap.put(EntityType.OCELOT, "passive-mobs.Ocelot");
-            entityBloodMap.put(EntityType.PARROT, "passive-mobs.Parrot");
-            entityBloodMap.put(EntityType.PIG, "passive-mobs.Pig");
-            entityBloodMap.put(EntityType.POLAR_BEAR, "passive-mobs.PolarBear");
-            entityBloodMap.put(EntityType.PUFFERFISH, "passive-mobs.Pufferfish");
-            entityBloodMap.put(EntityType.RABBIT, "passive-mobs.Rabbit");
-            entityBloodMap.put(EntityType.SALMON, "passive-mobs.Salmon");
-            entityBloodMap.put(EntityType.SHEEP, "passive-mobs.Sheep");
-            entityBloodMap.put(EntityType.SQUID, "passive-mobs.Squid");
-            entityBloodMap.put(EntityType.STRIDER, "passive-mobs.Strider");
-            entityBloodMap.put(EntityType.TROPICAL_FISH, "passive-mobs.TropicalFish");
-            entityBloodMap.put(EntityType.TURTLE, "passive-mobs.Turtle");
-            entityBloodMap.put(EntityType.VILLAGER, "passive-mobs.Villager");
-            entityBloodMap.put(EntityType.WANDERING_TRADER, "passive-mobs.WanderingTrader");
+            safelyAddEntityBloodType("ALLAY", "passive-mob.Wither");
+            safelyAddEntityBloodType("BAT", "passive-mobs.Bat");
+            safelyAddEntityBloodType("CAT", "passive-mobs.Cat");
+            safelyAddEntityBloodType("CHICKEN", "passive-mobs.Chicken");
+            safelyAddEntityBloodType("COD", "passive-mobs.Cod");
+            safelyAddEntityBloodType("COW", "passive-mobs.Cow");
+            safelyAddEntityBloodType("DONKEY", "passive-mobs.Donkey");
+            safelyAddEntityBloodType("FOX", "passive-mobs.Fox");
+            safelyAddEntityBloodType("GLOW_SQUID", "passive-mobs.GlowingSquid");
+            safelyAddEntityBloodType("HORSE", "passive-mobs.Horse");
+            safelyAddEntityBloodType("MULE", "passive-mobs.Mule");
+            safelyAddEntityBloodType("OCELOT", "passive-mobs.Ocelot");
+            safelyAddEntityBloodType("PARROT", "passive-mobs.Parrot");
+            safelyAddEntityBloodType("PIG", "passive-mobs.Pig");
+            safelyAddEntityBloodType("POLAR_BEAR", "passive-mobs.PolarBear");
+            safelyAddEntityBloodType("PUFFERFISH", "passive-mobs.Pufferfish");
+            safelyAddEntityBloodType("RABBIT", "passive-mobs.Rabbit");
+            safelyAddEntityBloodType("SALMON", "passive-mobs.Salmon");
+            safelyAddEntityBloodType("SHEEP", "passive-mobs.Sheep");
+            safelyAddEntityBloodType("SQUID", "passive-mobs.Squid");
+            safelyAddEntityBloodType("STRIDER", "passive-mobs.Strider");
+            safelyAddEntityBloodType("TROPICAL_FISH", "passive-mobs.TropicalFish");
+            safelyAddEntityBloodType("TURTLE", "passive-mobs.Turtle");
+            safelyAddEntityBloodType("VILLAGER", "passive-mobs.Villager");
+            safelyAddEntityBloodType("WANDERING_TRADER", "passive-mobs.WanderingTrader");
         }
-        if(plugin.getConfig().getBoolean("neutral.Enabled")){
-            entityBloodMap.put(EntityType.BEE, "neutral-mobs.Bee");
-            entityBloodMap.put(EntityType.DOLPHIN, "neutral-mobs.Dolphin");
-            entityBloodMap.put(EntityType.ENDERMAN, "neutral-mobs.Enderman");
-            entityBloodMap.put(EntityType.GOAT, "neutral-mobs.Goat");
-            entityBloodMap.put(EntityType.IRON_GOLEM,"neutral-mobs.IronGolem");
-            entityBloodMap.put(EntityType.LLAMA, "neutral-mobs.Llama");
-            entityBloodMap.put(EntityType.PANDA, "neutral-mobs.Panda");
-            entityBloodMap.put(EntityType.POLAR_BEAR,"neutral-mobs.PolarBear");
-            entityBloodMap.put(EntityType.TRADER_LLAMA, "neutral-mobs.TraderLlama");
-            entityBloodMap.put(EntityType.WOLF, "neutral-mobs.Wolf");
-            entityBloodMap.put(EntityType.ZOMBIFIED_PIGLIN, "neutral-mobs.ZombifiedPiglin");
+
+        if (plugin.getConfig().getBoolean("neutral.Enabled")) {
+            safelyAddEntityBloodType("BEE", "neutral-mobs.Bee");
+            safelyAddEntityBloodType("DOLPHIN", "neutral-mobs.Dolphin");
+            safelyAddEntityBloodType("ENDERMAN", "neutral-mobs.Enderman");
+            safelyAddEntityBloodType("GOAT", "neutral-mobs.Goat");
+            safelyAddEntityBloodType("IRON_GOLEM", "neutral-mobs.IronGolem");
+            safelyAddEntityBloodType("LLAMA", "neutral-mobs.Llama");
+            safelyAddEntityBloodType("PANDA", "neutral-mobs.Panda");
+            safelyAddEntityBloodType("POLAR_BEAR", "neutral-mobs.PolarBear");
+            safelyAddEntityBloodType("TRADER_LLAMA", "neutral-mobs.TraderLlama");
+            safelyAddEntityBloodType("WOLF", "neutral-mobs.Wolf");
+            safelyAddEntityBloodType("ZOMBIFIED_PIGLIN", "neutral-mobs.ZombifiedPiglin");
         }
-        if(plugin.getConfig().getBoolean("hostile.Enabled")){
-            entityBloodMap.put(EntityType.BLAZE, "hostile-mobs.Blaze");
-            entityBloodMap.put(EntityType.BREEZE, "hostile-mobs.Breeze");
-            entityBloodMap.put(EntityType.BOGGED, "hostile-mobs.Bogged");
-            entityBloodMap.put(EntityType.CREEPER, "hostile-mobs.Creeper");
-            entityBloodMap.put(EntityType.DROWNED, "hostile-mobs.Drowned");
-            entityBloodMap.put(EntityType.CAVE_SPIDER, "hostile-mobs.CaveSpider");
-            entityBloodMap.put(EntityType.ENDERMITE, "hostile-mobs.Endermite");
-            entityBloodMap.put(EntityType.EVOKER, "hostile-mobs.Evoker");
-            entityBloodMap.put(EntityType.GHAST, "hostile-mobs.Ghast");
-            entityBloodMap.put(EntityType.GUARDIAN, "hostile-mobs.Guardian");
-            entityBloodMap.put(EntityType.HOGLIN, "hostile-mobs.Hoglin");
-            entityBloodMap.put(EntityType.HUSK, "hostile-mobs.Husk");
-            entityBloodMap.put(EntityType.MAGMA_CUBE, "hostile-mobs.MagmaCube");
-            entityBloodMap.put(EntityType.PHANTOM, "hostile-mobs.Phantom");
-            entityBloodMap.put(EntityType.PIGLIN, "hostile-mobs.Piglin");
-            entityBloodMap.put(EntityType.PILLAGER, "hostile-mobs.Pillager");
-            entityBloodMap.put(EntityType.RAVAGER, "hostile-mobs.Ravager");
-            entityBloodMap.put(EntityType.SHULKER, "hostile-mobs.Shulker");
-            entityBloodMap.put(EntityType.SILVERFISH, "hostile-mobs.Silverfish");
-            entityBloodMap.put(EntityType.SKELETON, "hostile-mobs.Skeleton");
-            entityBloodMap.put(EntityType.SLIME, "hostile-mobs.Slime");
-            entityBloodMap.put(EntityType.SPIDER, "hostile-mobs.Spider");
-            entityBloodMap.put(EntityType.STRAY, "hostile-mobs.Stray");
-            entityBloodMap.put(EntityType.VEX, "hostile-mobs.Vex");
-            entityBloodMap.put(EntityType.VINDICATOR, "hostile-mobs.Vindicator");
-            entityBloodMap.put(EntityType.WITCH, "hostile-mobs.Witch");
-            entityBloodMap.put(EntityType.WITHER_SKELETON, "hostile-mobs.WitherSkeleton");
-            entityBloodMap.put(EntityType.ZOMBIE, "hostile-mobs.Zombie");
-            entityBloodMap.put(EntityType.ZOMBIE_VILLAGER, "hostile-mobs.ZombieVillager");
+
+        if (plugin.getConfig().getBoolean("hostile.Enabled")) {
+            safelyAddEntityBloodType("BLAZE", "hostile-mobs.Blaze");
+            safelyAddEntityBloodType("BREEZE", "hostile-mobs.Breeze");
+            safelyAddEntityBloodType("CREEPER", "hostile-mobs.Creeper");
+            safelyAddEntityBloodType("DROWNED", "hostile-mobs.Drowned");
+            safelyAddEntityBloodType("CAVE_SPIDER", "hostile-mobs.CaveSpider");
+            safelyAddEntityBloodType("ENDERMITE", "hostile-mobs.Endermite");
+            safelyAddEntityBloodType("EVOKER", "hostile-mobs.Evoker");
+            safelyAddEntityBloodType("GHAST", "hostile-mobs.Ghast");
+            safelyAddEntityBloodType("GUARDIAN", "hostile-mobs.Guardian");
+            safelyAddEntityBloodType("HOGLIN", "hostile-mobs.Hoglin");
+            safelyAddEntityBloodType("HUSK", "hostile-mobs.Husk");
+            safelyAddEntityBloodType("MAGMA_CUBE", "hostile-mobs.MagmaCube");
+            safelyAddEntityBloodType("PHANTOM", "hostile-mobs.Phantom");
+            safelyAddEntityBloodType("PIGLIN", "hostile-mobs.Piglin");
+            safelyAddEntityBloodType("PILLAGER", "hostile-mobs.Pillager");
+            safelyAddEntityBloodType("RAVAGER", "hostile-mobs.Ravager");
+            safelyAddEntityBloodType("SHULKER", "hostile-mobs.Shulker");
+            safelyAddEntityBloodType("SILVERFISH", "hostile-mobs.Silverfish");
+            safelyAddEntityBloodType("SKELETON", "hostile-mobs.Skeleton");
+            safelyAddEntityBloodType("SLIME", "hostile-mobs.Slime");
+            safelyAddEntityBloodType("SPIDER", "hostile-mobs.Spider");
+            safelyAddEntityBloodType("STRAY", "hostile-mobs.Stray");
+            safelyAddEntityBloodType("VEX", "hostile-mobs.Vex");
+            safelyAddEntityBloodType("VINDICATOR", "hostile-mobs.Vindicator");
+            safelyAddEntityBloodType("WITCH", "hostile-mobs.Witch");
+            safelyAddEntityBloodType("WITHER_SKELETON", "hostile-mobs.WitherSkeleton");
+            safelyAddEntityBloodType("ZOMBIE", "hostile-mobs.Zombie");
+            safelyAddEntityBloodType("ZOMBIE_VILLAGER", "hostile-mobs.ZombieVillager");
         }
-        if(plugin.getConfig().getBoolean("boss.Enabled")){
-            entityBloodMap.put(EntityType.ENDER_DRAGON, "boss-mobs.EnderDragon");
-            entityBloodMap.put(EntityType.ELDER_GUARDIAN, "boss-mobs.ElderGuardian");
-            entityBloodMap.put(EntityType.WARDEN, "boss-mobs.Warden");
-            entityBloodMap.put(EntityType.WITHER, "boss-mobs.Wither");
+
+        if (plugin.getConfig().getBoolean("boss.Enabled")) {
+            safelyAddEntityBloodType("ENDER_DRAGON", "boss-mobs.EnderDragon");
+            safelyAddEntityBloodType("ELDER_GUARDIAN", "boss-mobs.ElderGuardian");
+            safelyAddEntityBloodType("WARDEN", "boss-mobs.Warden");
+            safelyAddEntityBloodType("WITHER", "boss-mobs.Wither");
+        }
+    }
+
+    private void safelyAddEntityBloodType(String entityTypeName, String configPath) {
+        try {
+            EntityType entityType = EntityType.valueOf(entityTypeName);
+            entityBloodMap.put(entityType, configPath);
+        } catch (IllegalArgumentException e) {
+            plugin.getLogger().warning("EntityType " + entityTypeName + " is not available in this Minecraft version. Skipping...");
         }
     }
 
@@ -128,7 +139,7 @@ public class BloodEffect implements Listener {
         } catch (NullPointerException ex) {
             plugin.getLogger().warning("An entity or material could not be found or is null.");
         } catch (Exception ex) {
-            plugin.getLogger().severe("An unexpected error occurred while handling entity damage: " + ex.getMessage());
+            plugin.getLogger().severe("An unexpected error o2ccurred while handling entity damage: " + ex.getMessage());
             ex.printStackTrace();
         }
     }
